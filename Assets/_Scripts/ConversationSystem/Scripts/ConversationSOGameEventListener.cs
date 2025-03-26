5dde806a20d7dbfe3381e5c0ff06437cdd48de30
MatiasPhoenix <matias.la19@gmail.com>
2025-03-26 01:23:39 +0100

    second city scenario + more details first city scenario + dialog system (successful tests)

--- Assets/_Scripts/ConversationSystem/Scripts/ConversationSOGameEventListener.cs
+++ Assets/_Scripts/ConversationSystem/Scripts/ConversationSOGameEventListener.cs
@@ -1,9 +1,9 @@
-// using UnityEngine;
+using UnityEngine;
 
-// namespace ScriptableObjectArchitecture
-// {
-// 	[AddComponentMenu(SOArchitecture_Utility.EVENT_LISTENER_SUBMENU + "ConversationSO")]
-// 	public sealed class ConversationSOGameEventListener : BaseGameEventListener<ConversationSO, ConversationSOGameEvent, ConversationSOUnityEvent>
-// 	{
-// 	}
-// }
+namespace ScriptableObjectArchitecture
+{
+	[AddComponentMenu(SOArchitecture_Utility.EVENT_LISTENER_SUBMENU + "ConversationSO")]
+	public sealed class ConversationSOGameEventListener : BaseGameEventListener<ConversationSO, ConversationSOGameEvent, ConversationSOUnityEvent>
+	{
+	}
+}
